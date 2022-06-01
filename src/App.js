80e53bda4fa1b5e763fa9f57758fc111cd482e1d
stoneboat <tood2111@gmail.com>
2022-06-01 22:16:17 +0900

    Feat: 프로젝트 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import "./App.css";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className='App'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <div className="flex justify-center items-center h-screen bg-slate-200">
+      <div
+        className="border border-solid border-black bg-white w-[375px] h-[812px]"
+        style={{ fontFamily: 'ABeeZee', fontWeight: '700' }}
+      >
+        <Routes>
+          <Route path="/main" element={<Main />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
     </div>
   );
 }
