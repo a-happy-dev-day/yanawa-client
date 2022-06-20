fcf4fa488e715e471ce4e9bc7a55d15128b07211
stoneboat <tood2111@gmail.com>
2022-06-20 14:56:34 +0900

    Feat: tailwind -> mui 변경

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 import Main from './pages/Main';
 import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-slate-200">
-      <div
-        className="border border-solid border-black bg-white w-[375px] h-[812px]"
-        style={{ fontFamily: 'ABeeZee', fontWeight: '700' }}
+    <div style={{ backgroundColor: '#E8E8E8' }}>
+      <CssBaseline />
+      <Container
+        component="main"
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+        maxWidth={false}
       >
-        <Routes>
-          <Route path="/main" element={<Main />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
+        <Box sx={{ bgcolor: '#FFFFFF', width: '375px', height: '100vh' }}>
+          <Routes>
+            <Route path="/main" element={<Main />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Box>
+      </Container>
     </div>
   );
 }
